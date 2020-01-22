7868a702fabe514569a3005f45ac9d3c5bd29dea
Lauren Riddle <lari7132@gmail.com>
2020-01-21 18:40:58 -0600

    fixed all imports and opened the can'

--- appliances/__init__.py
+++ appliances/__init__.py
@@ -1,2 +1,10 @@
 from .appliance import Appliance
-from kitchen.utility.dishwasher import Dishwasher
+from .kitchen.utility.dishwasher import DishWasher
+from .kitchen.utility.refrigerator import Refrigerator
+from .laundry.dryer import Dryer
+from .laundry.washer import Washer
+from .kitchen.coffeemaker import CoffeeMaker
+from .kitchen.can_opener import CanOpener
+
+
+
--- index.py
+++ index.py
@@ -1,16 +1,22 @@
-from appliances import Dishwasher
-from appliances.laundry import Dryer
-from appliances.laundry import Washer
-from appliances.kitchen.utility import Refrigerator
+from appliances import Appliance
+from appliances import DishWasher
+from appliances import Washer
+from appliances import Dryer
+from appliances import Refrigerator
+from appliances import CoffeeMaker
+from appliances import CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
-samsung_washer = Washer("red")
+samsung_washer = Washer("red", "electric")
 samsung_dryer = Dryer("red", "gas")
 
 lg_fridge = Refrigerator("stainless")
 lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
-mr_coffee.make_coffee()
+mr_coffee.make_coffee()  
+
+can = CanOpener("blue")
+CanOpener.open_can()
