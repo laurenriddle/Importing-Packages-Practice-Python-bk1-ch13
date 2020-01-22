9c62aed7f5946acf5bc5b75df8562d6ebf4d67c7
Lauren Riddle <lari7132@gmail.com>
2020-01-21 18:41:42 -0600

    added comment

--- index.py
+++ index.py
@@ -5,7 +5,9 @@ from appliances import Dryer
 from appliances import Refrigerator
 from appliances import CoffeeMaker
 from appliances import CanOpener
-
+'''
+Clone this repo and refactor it to make it run error-free. Run index.py and use the error message to figure out where to start. Once the errors are gone, create a can opener and open a can (But hold your nose!)
+'''
 whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
